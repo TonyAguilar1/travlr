3dd5f704b55036914a2d6301e219de176838eea8
tony aguilar <juan.aguilar3@snhu.edu>
2026-09-13 15:50:57 -0500

    Module 2 completed baseline

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const crtMain = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', crtMain.index);
 
 module.exports = router;
