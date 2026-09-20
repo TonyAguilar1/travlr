8b2dd1d37f0da87f02ca1ceaf2eba339c76c67bb
tony aguilar <juan.aguilar3@snhu.edu>
2026-09-20 14:29:45 -0500

    Module 3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,10 @@
 /* GET travel view */
+
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 const travel = (req, res) => {
-  res.render('travel', { title: 'Travlr Gateways' });
+  res.render('travel', { title: 'Travlr Gateways', trips });
 };
 
 module.exports = {
